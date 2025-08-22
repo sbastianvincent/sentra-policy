9b39ea2a0b1d3ba00abf763455b45101d43f3634
Sebastian Soesilo <sbastianvincent@gmail.com>
2025-08-22 09:54:35 -0700

    modify spiffe to srn

--- src/main/java/com/svincent7/sentra/policy/config/PolicyConfig.java
+++ src/main/java/com/svincent7/sentra/policy/config/PolicyConfig.java
@@ -3,7 +3,7 @@ package com.svincent7.sentra.policy.config;
 import com.svincent7.sentra.common.auth.AuthService;
 import com.svincent7.sentra.common.auth.AuthServiceImpl;
 import com.svincent7.sentra.common.config.ConfigProperties;
-import com.svincent7.sentra.common.srn.SentraResourceNameImpl;
+import com.svincent7.sentra.common.srn.SentraResourceNameParserImpl;
 import com.svincent7.sentra.common.srn.SentraResourceNameParser;
 import com.svincent7.sentra.policy.audit.AuditClientConfig;
 import lombok.Data;
@@ -23,7 +23,7 @@ public class PolicyConfig implements ConfigProperties, AuditClientConfig {
 
     @Bean
     public SentraResourceNameParser sentraResourceNameParser() {
-        return new SentraResourceNameImpl();
+        return new SentraResourceNameParserImpl();
     }
 
     @Bean
